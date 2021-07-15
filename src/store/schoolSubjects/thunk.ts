3017187d1f4e1a01b343aded77b4fa6a635a3e46
dev1 <dev1@gmail.com>
2021-07-15 17:01:51 +0300

    Fix thunk schoolSubject

--- src/store/schoolSubjects/thunk.ts
+++ src/store/schoolSubjects/thunk.ts
@@ -3,7 +3,7 @@ import { ThunkAction } from 'redux-thunk'
 import { AppStore } from 'store/store'
 import { getSchoolSubjects } from 'services/schoolSubjects/schoolSubjects'
 
-export const fetchLessons =
+export const fetchSchoolSubjects =
   (): ThunkAction<Promise<void>, AppStore, unknown, any> => async dispatch => {
     const response = await getSchoolSubjects()
     dispatch(setSchoolSubjects(response.data))
