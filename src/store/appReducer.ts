33827646902df19ffd0acb0f03be4b98df7a89c5
dev1 <dev1@gmail.com>
2021-07-15 14:44:33 +0300

    Add schoolSubjectsReducer

--- src/store/appReducer.ts
+++ src/store/appReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import { lessonsReducer } from './lessons/reducer'
+import { schoolSubjectsReducer } from './schoolSubjects/reducer'
 
 export const appReducer = combineReducers({
   lessons: lessonsReducer,
+  schoolSubjects: schoolSubjectsReducer,
 })
