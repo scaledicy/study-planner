a20145b3a15304527c5644dd102f838b315ef858
dev1 <dev1@gmail.com>
2021-07-27 12:12:28 +0300

    Remove unused code

--- src/helpers/lessonHelpers.ts
+++ src/helpers/lessonHelpers.ts
@@ -20,35 +20,8 @@ export function lessonToForm(lesson: Lesson): LessonFormType {
   }
 }
 
-const parseTime = (timeString: string): number => {
-  if (timeString === '') return 0
-
-  const d = new Date('2000-01-01T' + timeString)
-
-  return d.getTime()
-}
-
-export const compareByStartTime = (lesson1: Lesson, lesson2: Lesson) => {
-  const start1 = parseTime(lesson1.start)
-  const start2 = parseTime(lesson2.start)
-
-  if (start1 < start2) {
-    return -1
-  } else if (start1 > start2) {
-    return 1
-  } else return 0
-}
-
 export const lessonsToTimeRecord = (lessons: Lesson[]): LessonTimeRecord =>
   lessons.reduce((res, lesson) => {
     res[lesson.start] = lesson
     return res
   }, {} as LessonTimeRecord)
-
-// export const lessonsToTimeRecord = (lessons: Lesson[]): LessonTimeRecord => {
-//   let obj:LessonTimeRecord = {}
-//   lessons.forEach(i => {
-//     obj[i.start] = i
-//   })
-//   return obj
-// }
